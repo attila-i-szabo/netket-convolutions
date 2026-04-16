137a516787a2a86cf22af087afe5b9a46b2ea2f0
Attila Szabo <szabo.attila94@gmail.com>
2026-04-16 20:09:55 +0200

    nomenclature

---
+++ netket_convolutions/__init__.py
@@ -0,0 +1,3 @@
+from ._base import default_equivariant_initializer
+from .symmetric_linear import DenseSymmMatrix, DenseSymmFFT, DenseSymmLAX
+from .equivariant_linear import EquivariantMatrix, EquivariantFFT, EquivariantLAX
